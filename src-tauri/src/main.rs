0952e699454accfcfc1de452681e9f2f85b668a0
Pasha Suprunchuk <suprunchuk@internet.ru>
2024-07-11 16:09:34 +0300

    [feat] new feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use palette::{Srgb, Lch};
-use palette::FromColor;
+
+use palette::{FromColor, Gradient, Lch, Srgb};
 
 fn main() {
     tauri::Builder::default()
@@ -10,13 +10,9 @@ fn main() {
 }
 
 #[tauri::command]
-fn generate_gradient(r: u8, g: u8, b: u8) {
+fn generate_gradient(r: u8, g: u8, b: u8) -> Vec<Vec<u8>> {
     println!("{} {} {}", r, g, b);
-    let my_rgb = Srgb::new(
-        r as f32 / 255.0,
-        g as f32 / 255.0,
-        b as f32 / 255.0,
-    );
+    let my_rgb = Srgb::new(r as f32 / 255.0, g as f32 / 255.0, b as f32 / 255.0);
     let my_lch: Lch = Lch::from_color(my_rgb.into_linear());
     let gradient = Gradient::new(vec![
         Lch::new(0.0, my_lch.chroma, my_lch.hue),
@@ -26,14 +22,9 @@ fn generate_gradient(r: u8, g: u8, b: u8) {
     let colors = gradient
         .take(10)
         .map(|color| {
-            let (r,g,b) = 
-                Srgb::from_color(color).into_components();
-            (
-                (r * 255.0) as u8, 
-                (g * 255.0) as u8, 
-                (b * 255.0) as u8,
-            )
-        }) 
-        .collect::<Vec< >>();
-    dbg!(my_lch);
+            let (r, g, b) = Srgb::from_color(color).into_components();
+            vec![(r * 255.0) as u8, (g * 255.0) as u8, (b * 255.0) as u8]
+        })
+        .collect::<Vec<_>>();
+    colors
 }
--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,30 @@ import { SketchPicker } from "react-color";
 
 function App() {
   const [color, setColor] = useState("1fa9f4");
+  const [gradient, setGradient] = useState([]);
   return (
     <div>
       <SketchPicker
         color={color}
         onChange={(color, event) => {
           setColor(color);
-          console.log(color)
-          invoke("generate_gradient", color.rgb)
+          console.log(color);
+          invoke("generate_gradient", color.rgb).then((grad) => {
+            console.log(grad);
+            setGradient(grad);
+          });
         }}
       />
+      {gradient.map((color) => (
+        <div
+          style={{
+            padding: "1rem",
+            background: `rgb(${color[0]}, ${color[1]}, ${color[2]})`,
+          }}
+        >
+          rgb({color[0]}, {color[1]}, {color[2]})
+        </div>
+      ))}
     </div>
   );
 }
